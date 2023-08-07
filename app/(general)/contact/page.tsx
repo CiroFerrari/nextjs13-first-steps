3779a3600b0aa2849d310d6cca38bd17586a155d
Franco Ciro Ferrari <francociroferrari@gmail.com>
2023-08-07 12:50:11 -0300

    First commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Contact Page",
+  description: 'Contacto de la empresa',
+  keywords: ['Contact page', 'Franco', 'contacto', '...'],
+}
+
+export default function ContactPage() {
+  return (
+    <span className="text-7xl">Contact Page</span>
+  )
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Pricing Page",
+  description: 'Aquí están los precios',
+  keywords: ['Pricing page', 'Franco', 'precios', '...'],
+}
+
+export default function PricingPage() {
+  return (
+    <span className="text-7xl">Pricing Page</span>
+  )
+}
